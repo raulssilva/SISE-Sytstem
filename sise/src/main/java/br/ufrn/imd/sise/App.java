b05fe24b5a4013a80fb6b69ffa51b25d5937a295
Felipe Barbalho <felipebarbalho.95@gmail.com>
2016-08-20 14:21:02 -0300

    Create java maven project

---
+++ sise/src/main/java/br/ufrn/imd/sise/App.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.sise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
