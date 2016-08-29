779f9eb50b46f5da89e8ea5fcd8095e173d60239
Felipe Barbalho <felipebarbalho.95@gmail.com>
2016-08-29 11:14:55 -0300

    create unit tests structs complete

---
+++ sise/src/test/java/br/ufrn/imd/sise/model/NewsTest.java
@@ -0,0 +1,14 @@
+package br.ufrn.imd.sise.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
