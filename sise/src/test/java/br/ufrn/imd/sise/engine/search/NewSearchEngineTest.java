9c8b8b7b2f15be526d064abc9e2e742d02a2329d
Felipe Barbalho <felipebarbalho.95@gmail.com>
2016-08-29 11:04:27 -0300

    create unit tests structs

---
+++ sise/src/test/java/br/ufrn/imd/sise/engine/search/NewSearchEngineTest.java
@@ -0,0 +1,14 @@
+package br.ufrn.imd.sise.engine.search;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewSearchEngineTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
