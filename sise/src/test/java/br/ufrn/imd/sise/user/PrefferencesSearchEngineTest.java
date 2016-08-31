76bef122ede1136effa3c02bb2916f6da6f52854
Felipe Barbalho <felipebarbalho.95@gmail.com>
2016-08-30 23:22:14 -0300

    Implements PrefferencesSearchEngineTest

---
+++ sise/src/test/java/br/ufrn/imd/sise/user/PrefferencesSearchEngineTest.java
@@ -0,0 +1,14 @@
+package br.ufrn.imd.sise.user;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrefferencesSearchEngineTest {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
