f682d9aecf64d00c9e9cb40eb1ce06a07a4b8615
Raul Silveira <raul-brasil@hotmail.com>
2016-09-23 15:27:31 -0300

    [In Progress] - Listing terms of News.

--- sise/src/main/java/br/ufrn/imd/sise/engine/filter/Filter.java
+++ sise/src/main/java/br/ufrn/imd/sise/engine/filter/Filter.java
@@ -1,6 +1,21 @@
 package br.ufrn.imd.sise.engine.filter;
 
-public class Filter {
-	
+import java.util.List;
 
+import br.ufrn.imd.sise.engine.model.Information;
+import br.ufrn.imd.sise.user.model.Prefferences;
+
+public class Filter {
+	public List<Information> filtrar(List<Information> listInformation, Prefferences prefferences){
+		List<Information> filtredList = null;
+		return filtredList;
+	}
+	
+	private List<String> getListTerms(List<Information> listInformation){
+		List<String> listTerms = null;
+		for (Information information : listInformation) {
+			String content = information.getContent();
+		}
+		return listTerms;
+	}
 }
