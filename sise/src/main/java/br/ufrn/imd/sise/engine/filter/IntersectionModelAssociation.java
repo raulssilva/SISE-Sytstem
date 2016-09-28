907d5474e6b4c1e628631f50ff1413fc2da595aa
Jackson Rauup <jackson.rauup.info@gmail.com>
2016-09-28 04:15:54 -0300

    Update the IntersectionModelAssociation class.

--- sise/src/main/java/br/ufrn/imd/sise/engine/filter/IntersectionModelAssociation.java
+++ sise/src/main/java/br/ufrn/imd/sise/engine/filter/IntersectionModelAssociation.java
@@ -4,6 +4,12 @@ import br.ufrn.imd.sise.engine.filter.model.UserPreferences;
 
 public class IntersectionModelAssociation implements ModelAssociation{
 
+	private static IntersectionModelAssociation intersectionModelAssociation = new IntersectionModelAssociation();
+	
+	public IntersectionModelAssociation(){
+		
+	}
+	
 	/* Comparation method.
 	 * return:
 	 * 10 - if the two terms are equals.
@@ -13,30 +19,41 @@ public class IntersectionModelAssociation implements ModelAssociation{
 	@Override
 	public double calculate(UserPreferences termPreferences, String termsInformation) {
 		
-		// TODO
-		String userTerm = termPreferences.getTermo(); 
-		if(userTerm.equals(termsInformation)) {
-			return 10.0;
-		} else {
-			double value = 0, intersec = 0;
-			int size = 0;	
-			if(userTerm.length() > termsInformation.length()) {
-				size = userTerm.length(); 
+		try {
+			// TODO
+			String userTerm = termPreferences.getTermo(); 
+			if(userTerm.equals(termsInformation)) {
+				return 10.0;
 			} else {
-				size = termsInformation.length();
-			} 
-			
-			for(int i=0; i<size;i++) {
-				if(userTerm.charAt(i) == termsInformation.charAt(i)) {
-					intersec += 1;
+				double value = 0, intersec = 0;
+				int size = 0;	
+				if(userTerm.length() > termsInformation.length()) {
+					size = userTerm.length(); 
 				} else {
-					value = ((intersec/userTerm.length()) * 10);
-					return value;
+					size = termsInformation.length();
+				} 
+				
+				for(int i=0; i<size;i++) {
+					if(userTerm.charAt(i) == termsInformation.charAt(i)) {
+						intersec += 1;
+					} else {
+						value = ((intersec/userTerm.length()) * 10);
+						return value;
+					}
 				}
-			}
+				
+				return value;
+			}	
 			
-			return value;
-		}		
+		} catch (Exception e){
+			e.getStackTrace();
+		}
+		
+		return 0;
 	}	
+	
+	public static IntersectionModelAssociation getInstance() {
+		return intersectionModelAssociation;
+	}
 
 }
